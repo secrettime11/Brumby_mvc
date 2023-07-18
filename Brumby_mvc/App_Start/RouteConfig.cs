f66762552d869d070acabee5a76be82074951205
Brumby <secrettime11@gmail.com>
2023-07-18 16:59:38 +0800

    0718 改layout menu

--- Brumby_mvc/App_Start/RouteConfig.cs
+++ Brumby_mvc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Brumby_mvc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Stock", action = "OTC", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
